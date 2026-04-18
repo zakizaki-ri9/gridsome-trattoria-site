a4eba8c8136daf619cc3557a3156b7b8e07cdac3
zakizaki-ri9 <kic.yuuki@gmail.com>
2026-04-18 10:33:02 +0900

    fix(analytics): GA4のトラッキングコードを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag'],
+      },
+    }),
+  ],
+});
